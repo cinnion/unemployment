ea1bb46d2bb8d2b7578a06ee811ee11e1bf3a524
Douglas Needham <cinnion@gmail.com>
2025-01-17 16:35:45 -0500

    Add edit button and adjust detail view
    - Add a placeholder column to allow for editing.
    - Define function to customize the placehold actions column.
    - Turn off full_row_select so that the edit button works instead of calling
      up the details.
    - Define function to override the details view rendering.

--- applications/ajax_datatable_views.py
+++ applications/ajax_datatable_views.py
@@ -20,10 +20,25 @@ class ApplicationsAjaxDatatableView(AjaxDatatableView):
         {'name': 'company', 'visible': True, },
         {'name': 'title', 'visible': True, },
         {'name': 'active', 'visible': True, },
-        # {'name': 'actions', 'title': 'Actions', 'placeholder': True, 'searchable': False, 'orderable': False, },
+        {'name': 'actions', 'title': 'Actions', 'placeholder': True, 'searchable': False, 'orderable': False, },
     ]
 
     def customize_row(self, row, obj):
-        row['Actions'] = """
-            <a href="{% url 'applications:application-details' appid=app.id %}"><button>View/Edit</button></a>
-        """
+        row['actions'] = """
+                    <a href="#"
+                    onclick="var id=this.closest('tr').id.substr(4); window.location.href='/applications/' + id + '/edit'"><button>Edit</button></a>
+                """
+
+    def render_row_details(self, pk, request=None):
+        obj = self.model.objects.get(pk=pk)
+        fields = [f for f in self.model._meta.get_fields() if f.concrete]
+        html = '<table class="row-details">'
+        for field in fields:
+            value = getattr(obj, field.name)
+            html += '<tr><td>%s</td>' % field.name
+            if field.name in [ 'posting', 'confirm'] and value is not None:
+                html += '<td><a href="%s" target="_blank">%s</a></td></tr>' % (value, value)
+            else:
+                html += '<td>%s</td></tr>' % value
+        html += '</table>'
+        return html
