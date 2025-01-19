81cebe7ba2540f2435d44ae15efa9f52502e41c9
Douglas Needham <cinnion@gmail.com>
2025-01-18 20:11:30 -0500

    Get sorting and edit button working
    - Create method in the view to parse the order information from jQuery
      DataTables.
    - Add call to new method and sort the dataset if we have sorting specified.
    - Define a rowId for the data, which can be used for requesting details or
      editing a record.
    - Fix the edit button.
    - Add back the columns we deleted but make them hidden. This will allow us to
      potentially change column visibility for these columns.

--- applications_api/views.py
+++ applications_api/views.py
@@ -1,12 +1,40 @@
+from typing import List, Optional
+
 from rest_framework.response import Response
 from rest_framework import status, generics
 from applications.models import JobApplication
 from applications_api.serializers import JobApplicationSerializer
 from datetime import date
 
+
 class JobApplications(generics.GenericAPIView):
     serializer_class = JobApplicationSerializer
     queryset = JobApplication.objects.all()
+
+    def get_sort(self, request) -> Optional[List[str]]:
+        """
+        Parse the request to get the ordering information and return it in a Django compliant list.
+        The method returns None if there is no sorting requested.
+
+        :param request:
+        :return:
+        """
+        sort_order = []
+        i = 0
+        name = request.GET.get("order[%i][name]" % i)
+        while name is not None:
+            direction = request.GET.get("order[%i][dir]" % i)
+            if direction == 'desc':
+                sort_order.append('-' + name)
+            else:
+                sort_order.append(name)
+            i += 1
+            name = request.GET.get("order[%i][name]" % i)
+
+        if len(sort_order) == 0:
+            sort_order = None
+
+        return sort_order
 
     def get(self, request):
         draw = int(request.GET.get("draw", 1))
@@ -17,10 +45,14 @@ class JobApplications(generics.GenericAPIView):
         job_applications = JobApplication.objects.all()
         total_applications = job_applications.count()
         total_filtered = total_applications
+        sort_order = self.get_sort(request)
 
         if search_param:
             job_applications = job_applications.filter(company__icontains=search_param)
             total_filtered = job_applications.count()
+
+        if sort_order:
+            job_applications = job_applications.order_by(*sort_order)
 
         serializer = self.serializer_class(job_applications[start_num:end_num], many=True)
 
